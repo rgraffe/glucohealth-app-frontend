5673fbee13b7344290d0ae065a6e66adb3dc52c6
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-04-22 22:30:56 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GlucoHealth',
+  webDir: 'dist'
+};
+
+export default config;
