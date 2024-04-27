92b743742a61b9a74f3fd8d1c195d51e2b69b83b
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-04-26 23:03:34 -0400

    feat: header, searchbar and fab button

--- src/pages/patients/page.tsx
+++ src/pages/patients/page.tsx
@@ -4,8 +4,13 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonText,
+  IonIcon,
+  IonSearchbar,
+  IonFab,
+  IonFabButton,
 } from '@ionic/react'
-import ExploreContainer from '~/shared/components/ExploreContainer'
+import { add, search } from 'ionicons/icons'
 
 export function PatientsPage() {
   return (
@@ -16,7 +21,24 @@ export function PatientsPage() {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <ExploreContainer name="Patients page" />
+        <main className="w-full h-full flex flex-col items-center pt-4">
+          <header className="w-full flex flex-col items-center justify-center">
+            <IonText className="text-center">
+              <h1 className="text-3xl">Pacientes</h1>
+            </IonText>
+            <div className="w-full mt-5 flex flex-col items-center max-w-xl px-8">
+              <p className="text-lg">Busque a un paciente</p>
+              <IonSearchbar placeholder="Número único de identificación"></IonSearchbar>
+            </div>
+          </header>
+          <div className="w-full m-6 h-[1px] bg-black dark:bg-white" />
+
+          <IonFab slot="fixed" horizontal="end" vertical="bottom" className='mb-16 mr-2'>
+            <IonFabButton>
+              <IonIcon icon={add}></IonIcon>
+            </IonFabButton>
+          </IonFab>
+        </main>
       </IonContent>
     </IonPage>
   )
