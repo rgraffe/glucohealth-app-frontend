1b13b6fa62570f6e3c59ac7cfbd4e663a88f08e9
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-04-27 13:47:04 -0400

    feat: add horizontal gap

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -6,7 +6,7 @@ export function LoginPage() {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <main className="w-full h-full flex justify-center items-center flex-col gap-5">
+        <main className="w-full h-full flex justify-center items-center flex-col gap-5 px-5">
           <IonText className="text-center">
             <h1 className="text-4xl">GlucoHealth</h1>
             <h2>Bienvenido, enfermero/a</h2>
