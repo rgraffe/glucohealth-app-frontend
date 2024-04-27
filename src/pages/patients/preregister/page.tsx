add4a5468c4d663919efae387ea2980cfa250feb
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-04-27 13:54:30 -0400

    feat: text in preregister page

--- src/pages/patients/preregister/page.tsx
+++ src/pages/patients/preregister/page.tsx
@@ -4,6 +4,7 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonText,
 } from '@ionic/react'
 import { PreregisterForm } from './components/preregister-form'
 
@@ -16,7 +17,17 @@ export function PatientPreregisterPage() {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <main className="w-full h-full flex justify-center items-center px-5">
+        <main className="w-full h-full flex flex-col justify-center items-center px-5 gap-8">
+          <header className="flex flex-col w-full items-center">
+            <IonText className="text-center w-4/5">
+              <p>
+                Para prerregistrar a un paciente solo necesitas su correo
+                electrónico. Cuando el paciente inicie sesión en la aplicación
+                deberá ingresar sus datos personales.
+              </p>
+            </IonText>
+          </header>
+
           <PreregisterForm />
         </main>
       </IonContent>
