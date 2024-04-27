320160c6749fbc131392cff59f0c2e885f9b04df
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-04-27 11:52:07 -0400

    feat: type patient entity

---
+++ src/shared/types/patient.ts
@@ -0,0 +1,11 @@
+export interface Patient {
+  id: string
+  fullName: string
+  email: string
+  phoneNumber: string
+  nationalId: string
+  age: number
+  weightInKg: number
+  heightInCm: number
+  password: string
+}
