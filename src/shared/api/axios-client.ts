6d2b3378fae16b8b4859feb00bdf5b61ae561570
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-05-10 19:24:30 -0400

    feat: axios params serializer

--- src/shared/api/axios-client.ts
+++ src/shared/api/axios-client.ts
@@ -1,7 +1,28 @@
 import axios from 'axios'
 import { BACKEND_BASE_URL } from '~/shared/config'
 
+const convertKeysToKebabCase = (
+  obj: Record<string, any>,
+): Record<string, any> => {
+  const kebabCaseObj: Record<string, any> = {}
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const kebabCaseKey = key
+        .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+        .toLowerCase()
+      kebabCaseObj[kebabCaseKey] = obj[key]
+    }
+  }
+  return kebabCaseObj
+}
+
 export const axiosClient = axios.create({
   baseURL: BACKEND_BASE_URL,
   responseType: 'json',
+  paramsSerializer: params => {
+    const kebabCaseParams = convertKeysToKebabCase(params)
+    return Object.entries(kebabCaseParams)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&')
+  },
 })
