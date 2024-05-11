4a2b94afad08508e63e90336d680838cd545c317
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-05-10 20:08:33 -0400

    fix: infinite scroll

--- src/pages/patients/components/patients-list/patients-list.tsx
+++ src/pages/patients/components/patients-list/patients-list.tsx
@@ -9,26 +9,22 @@ import { getPaginatedPatients } from '~/features/patients/services/get-paginated
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { QUERY_KEYS } from '~/features/patients/constants'
 
+const INITIAL_PAGE = 1
 const ITEMS_PER_PAGE = 5
 
 export function PatientsList() {
-  const { data, isPending, isFetching, isError, fetchNextPage } =
-    useInfiniteQuery({
-      queryKey: [QUERY_KEYS.PATIENTS_LIST],
-      queryFn: ({ pageParam }) => {
-        console.log('pageParam', pageParam)
-        return getPaginatedPatients({
-          pageIndex: pageParam,
-          itemsPerPage: ITEMS_PER_PAGE,
-        })
-      },
-      initialPageParam: 0,
-      getNextPageParam: lastPage => lastPage.pageIndex + 1,
-    })
-
-  if (isPending || isFetching) {
-    return <h1>Cargando</h1>
-  }
+  const { data, isError, fetchNextPage } = useInfiniteQuery({
+    queryKey: [QUERY_KEYS.PATIENTS_LIST],
+    queryFn: ({ pageParam }) => {
+      console.log('pageParam', pageParam)
+      return getPaginatedPatients({
+        pageIndex: pageParam,
+        itemsPerPage: ITEMS_PER_PAGE,
+      })
+    },
+    initialPageParam: INITIAL_PAGE,
+    getNextPageParam: lastPage => lastPage.pageIndex + 1,
+  })
 
   if (isError) {
     return <h1>Error listando los clientes.</h1>
@@ -37,7 +33,7 @@ export function PatientsList() {
   return (
     <>
       <IonList className="overflow-y-scroll max-w-xl gap-5 w-full">
-        {data.pages.map(p => (
+        {data?.pages?.map(p => (
           <Fragment key={p.pageIndex}>
             {p.items.map(patient => (
               <PatientListItem key={patient.id} patient={patient} />
@@ -46,9 +42,9 @@ export function PatientsList() {
         ))}
       </IonList>
       <IonInfiniteScroll
-        onIonInfinite={ev => {
-          fetchNextPage()
-          setTimeout(() => ev.target.complete(), 500)
+        onIonInfinite={async ev => {
+          await fetchNextPage()
+          ev.target.complete()
         }}
       >
         <IonInfiniteScrollContent></IonInfiniteScrollContent>
