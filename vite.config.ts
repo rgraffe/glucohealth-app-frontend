3be35bd2ec8781dc8eb7b33ba3569b5295946358
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-05-14 12:17:59 -0400

    feat: vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy(), tsConfigPaths()],
+  plugins: [
+    react(),
+    legacy(),
+    tsConfigPaths(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
