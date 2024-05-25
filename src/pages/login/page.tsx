cf130ac9df38498eb9e596fcc7efd0bf8b4f7386
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-05-25 19:06:50 -0400

    feat: dark version of logo

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,14 +1,26 @@
 import { IonPage, IonContent, IonText } from '@ionic/react'
 import { LoginForm } from './components/login-form'
 import logo from '~/shared/assets/logo.png'
+import darkLogo from '~/shared/assets/logo-dark.png'
 
 export function LoginPage() {
+  let isDarkMode = matchMedia('(prefers-color-scheme: dark)').matches
+
   return (
     <IonPage>
       <IonContent fullscreen>
         <main className="w-full h-full flex justify-center items-center flex-col gap-5 px-5">
           <IonText className="text-center flex flex-col items-center">
-            <img src={logo} alt="GlucoHealth" className="w-[90%] max-w-xl" />
+            {isDarkMode ? (
+              <img
+                src={darkLogo}
+                alt="GlucoHealth"
+                className="w-[90%] max-w-xl"
+              />
+            ) : (
+              <img src={logo} alt="GlucoHealth" className="w-[90%] max-w-xl" />
+            )}
+
             <h2>Bienvenido, enfermero/a</h2>
           </IonText>
 
